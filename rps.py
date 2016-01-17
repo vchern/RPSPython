03fa5d1f622f3f3aa16fe8723317b8fadc6805bc
vchern <victor.chern@yahoo.ca>
2016-01-16 18:33:41 -0800

    Commented code

--- rps.py
+++ rps.py
@@ -1,11 +1,13 @@
 from random import randint
 
+# Global variables for the player's record
 playerLoss = 0
 playerWin = 0
 draw = 0
 
 while 1:
 
+	# Generate computer's choice of RPS
 	compChoiceNum = randint(1,3)
 	if compChoiceNum == 1:
 		compChoice = "Rock"
@@ -14,6 +16,7 @@ while 1:
 	if compChoiceNum == 3:
 		compChoice = "Scissors"
 
+	# Ask user for choice
 	choice = input("Rock, Paper or Scissors? ")
 
 	if choice == "Rock":
@@ -26,6 +29,7 @@ while 1:
 		print("Sorry, that's not an option!")
 		continue
 
+	# Compare users choice against computer's choice
 	if choiceNum-compChoiceNum == 0:
 		print("Computer chose {0} and you chose {1}. It's a draw".format(compChoice, choice))
 		draw += 1
@@ -58,6 +62,7 @@ while 1:
 			playerLoss += 1
 			print("Score: {0} wins, {1} losses, {2} draws.".format(playerWin, playerLoss, draw))
 
+	# Ask user to replay
 	again = input("Keep playing? (y/n): ")
 	if again == "y":
 		continue
